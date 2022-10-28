60cb116a14fd9c3b45b7ac7e305caa341cb37628
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-10-28 17:59:17 +0800

    fix jwt user service errors

--- backend/src/repositories/user-credential.repository.ts
+++ backend/src/repositories/user-credential.repository.ts
@@ -2,12 +2,6 @@ import {inject} from '@loopback/core';
 import {DefaultCrudRepository} from '@loopback/repository';
 import {MongodbDataSource} from '../datasources';
 import {UserCredential, UserCredentialRelations} from '../models';
-
-export type Credentials = {
-  email: string;
-  password: string;
-};
-
 export class UserCredentialRepository extends DefaultCrudRepository<
   UserCredential,
   typeof UserCredential.prototype.id,
