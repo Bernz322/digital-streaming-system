dbd2f712220f965d7ca2c1a34dd24685ca2f7729
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-10-30 13:43:11 +0800

    add actors controller and partial functionalities

---
+++ backend/src/controllers/actors.controller.ts
@@ -0,0 +1,203 @@
+import {authenticate} from '@loopback/authentication';
+import {authorize} from '@loopback/authorization';
+import {Filter, FilterExcludingWhere, repository} from '@loopback/repository';
+import {
+  post,
+  param,
+  get,
+  getModelSchemaRef,
+  patch,
+  del,
+  requestBody,
+  response,
+} from '@loopback/rest';
+import {Actors} from '../models';
+import {ActorsRepository} from '../repositories';
+import {CustomResponse, validateName} from '../utils';
+
+export class ActorsController {
+  constructor(
+    @repository(ActorsRepository)
+    public actorsRepository: ActorsRepository,
+  ) {}
+
+  // TODO: Add search actor by name endpoint
+
+  @authenticate('jwt')
+  @authorize({allowedRoles: ['admin']})
+  @post('/actors')
+  @response(200, {
+    description:
+      'Add new actor and return actor created data. (Requires token and admin role authorization)',
+    content: {'application/json': {schema: getModelSchemaRef(Actors)}},
+  })
+  async create(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Actors, {
+            title: 'NewActors',
+            exclude: ['id'],
+          }),
+        },
+      },
+    })
+    actors: Actors,
+  ): Promise<CustomResponse<{}>> {
+    try {
+      validateName(actors.firstName, 'firstName');
+      validateName(actors.lastName, 'lastName');
+      if (actors.gender !== 'male' && actors.gender !== 'female')
+        throw new Error('Gender should only be either male or female');
+      if (actors.age < 1)
+        throw new Error('Actor age cannot be less than a year.');
+
+      const actorCreated = await this.actorsRepository.create(actors);
+      return {
+        status: 'success',
+        data: actorCreated,
+        message: 'Successfully added a new actor.',
+      };
+    } catch (error) {
+      return {
+        status: 'fail',
+        data: null,
+        message: error ? error.message : 'Adding new Actor failed.',
+      };
+    }
+  }
+
+  @get('/actors')
+  @response(200, {
+    description: 'Return array of all actors in the database.',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'array',
+          items: getModelSchemaRef(Actors, {includeRelations: true}),
+        },
+      },
+    },
+  })
+  async find(
+    @param.filter(Actors) filter?: Filter<Actors>,
+  ): Promise<CustomResponse<{}>> {
+    try {
+      const actors = await this.actorsRepository.find(filter);
+      return {
+        status: 'success',
+        data: actors,
+        message: 'Successfully fetched all actors in the database.',
+      };
+    } catch (error) {
+      return {
+        status: 'fail',
+        data: null,
+        message: error ? error.message : 'Fetching all actors failed.',
+      };
+    }
+  }
+
+  @get('/actors/{id}')
+  @response(200, {
+    description: 'Return all data of an actor (provide actor id).',
+    content: {
+      'application/json': {
+        schema: getModelSchemaRef(Actors, {includeRelations: true}),
+      },
+    },
+  })
+  async findById(
+    @param.path.string('id') id: string,
+    @param.filter(Actors, {exclude: 'where'})
+    filter?: FilterExcludingWhere<Actors>,
+  ): Promise<CustomResponse<{}>> {
+    try {
+      const actorData = await this.actorsRepository.findById(id, filter);
+      return {
+        status: 'success',
+        data: actorData,
+        message: 'Successfully fetched actor data.',
+      };
+    } catch (error) {
+      return {
+        status: 'success',
+        data: null,
+        message: 'Fetching actor data failed.',
+      };
+    }
+  }
+
+  @authenticate('jwt')
+  @authorize({allowedRoles: ['admin']})
+  @patch('/actors/{id}')
+  @response(204, {
+    description: 'Actors PATCH success',
+  })
+  async updateById(
+    @param.path.string('id') id: string,
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Actors, {
+            title: 'Update actor',
+            exclude: ['id'],
+            partial: true,
+          }),
+        },
+      },
+    })
+    actors: Actors,
+  ): Promise<CustomResponse<{}>> {
+    // TODO: Populate actor casted movies and return it also.
+    try {
+      validateName(actors.firstName, 'firstName');
+      validateName(actors.lastName, 'lastName');
+      if (actors.gender !== 'male' && actors.gender !== 'female')
+        throw new Error('Gender should only be either male or female');
+      if (actors.age < 1)
+        throw new Error('Actor age cannot be less than a year.');
+
+      await this.actorsRepository.updateById(id, actors);
+      const updatedActor = await this.actorsRepository.findById(id);
+
+      return {
+        status: 'success',
+        data: updatedActor,
+        message: 'Actor updated successfully',
+      };
+    } catch (error) {
+      return {
+        status: 'fail',
+        data: null,
+        message: error ? error.message : 'Updating actor failed.',
+      };
+    }
+  }
+
+  @authenticate('jwt')
+  @authorize({allowedRoles: ['admin']})
+  @del('/actors/{id}')
+  @response(204, {
+    description: 'Actors DELETE success',
+  })
+  async deleteById(
+    @param.path.string('id') id: string,
+  ): Promise<CustomResponse<{}>> {
+    // TODO: Check if actor has a movie where he/she casted ad do not delete actor if it has.
+    try {
+      await this.actorsRepository.deleteById(id);
+      return {
+        status: 'success',
+        data: id,
+        message: 'Actor deleted successfully failed.',
+      };
+    } catch (error) {
+      return {
+        status: 'fail',
+        data: null,
+        message: error ? error.message : 'Deleting actor failed.',
+      };
+    }
+  }
+}
