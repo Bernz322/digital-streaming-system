90fa6859be999ff590440567a97c7a6c59980eab
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-11-01 19:27:27 +0800

    add configs inside app.tsx and setup routing

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,5 @@
+// export { default as Button } from "./Button/Button";
+export { default as Navbar } from "./Navbar/Navbar";
+export { default as Hero } from "./Hero/Hero";
+export { default as MoviesSection } from "./MoviesSection/MoviesSection";
+export { default as MovieCard } from "./MovieCard/MovieCard";
