9205ed03ae6f9c21ad35a6968d6f30e8843af733
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-11-02 14:51:40 +0800

    add movie rating reusable component

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -3,3 +3,5 @@ export { default as Hero } from "./Hero/Hero";
 export { default as MoviesSection } from "./MoviesSection/MoviesSection";
 export { default as MovieCard } from "./MovieCard/MovieCard";
 export { default as ActorCard } from "./ActorCard/ActorCard";
+export { default as Rating } from "./Rating/Rating";
+export { default as ReviewCard } from "./ReviewCard/ReviewCard";
