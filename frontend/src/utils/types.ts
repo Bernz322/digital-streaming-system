665577ad5fbd5bc7b8065ba9a5cba36f6e583668
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-11-02 22:16:30 +0800

    add more types for api requests

--- frontend/src/utils/types.ts
+++ frontend/src/utils/types.ts
@@ -2,15 +2,36 @@ export interface ILoginResponse {
   status: string;
   data: {
     token: string;
-    user: {
-      id: string;
-      name: string;
-      email: string;
-      role: string;
-      isActivated: boolean;
-    };
+    user: IUserLogin;
   };
   message: string;
+}
+export interface APICustomResponse<T> {
+  status: string;
+  data?: T | T[] | null;
+  message?: string;
+}
+export interface IUserLogin {
+  id: string;
+  name: string;
+  email: string;
+  role: string;
+  isActivated: boolean;
+}
+export interface IRegisterAPIProps {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+export interface IUser {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  role: string;
+  isActivated: boolean;
+  dateCreated: string;
 }
 export interface IMovie {
   id: string;
@@ -29,4 +50,12 @@ export interface IActor {
   image: string;
   link: string;
   moviesCasted?: number;
+}
+export interface IDispatchResponse {
+  error?: {
+    message?: string;
+  };
+  meta: any;
+  payload: any;
+  type: string;
 }
