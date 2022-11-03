bbd98a90dbc296ff8c9bfe648964835c097bc96a
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-11-03 12:31:52 +0800

    add fetching of movies by id and render those data

--- frontend/src/pages/IndividualMovie.tsx
+++ frontend/src/pages/IndividualMovie.tsx
@@ -1,97 +1,39 @@
 import { Carousel } from "@mantine/carousel";
 import { Button, Rating, Textarea } from "@mantine/core";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { ActorCard, Rating as MyRating, ReviewCard } from "../components";
-import { useTypedSelector } from "../hooks/rtk-hooks";
+import { fetchMovieById, postMovieReview } from "../features/movie/movieSlice";
+import { useTypedDispatch, useTypedSelector } from "../hooks/rtk-hooks";
 import { budgetFormatter } from "../utils/helpers";
-import { IActor, IMovie } from "../utils/types";
 
 const IndividualMovie = () => {
   const { loggedIn } = useTypedSelector((state) => state.auth);
+  const { selectedMovie } = useTypedSelector((state) => state.movie);
+  const dispatch = useTypedDispatch();
+  const { id } = useParams();
   const [reviewComment, setReviewComment] = useState<string>("");
   const [reviewRating, setReviewRating] = useState<number>(3);
+  const [error, setError] = useState<boolean>(false);
+
+  useEffect(() => {
+    dispatch(fetchMovieById(id as string));
+  }, [dispatch, id]);
 
   const handleMovieReviewSubmit = useCallback(() => {
-    console.log({ reviewComment, reviewRating });
-  }, [reviewComment, reviewRating]);
+    if (reviewComment === "") return setError(true);
+    const reviewData = {
+      description: reviewComment,
+      rating: reviewRating,
+      movieId: id as string,
+    };
+    dispatch(postMovieReview(reviewData));
+  }, [reviewComment, reviewRating, id, dispatch]);
 
-  const actors: IActor[] = [
-    {
-      id: "6360ea98e7653b1108a2397703",
-      firstName: "Joel",
-      lastName: "Edgerton",
-      gender: "male",
-      age: 48,
-      image:
-        "https://m.media-amazon.com/images/M/MV5BMTA0ODI1ODk4NzdeQTJeQWpwZ15BbWU3MDkwNjkzOTY@._V1_.jpg",
-      link: "https://www.imdb.com/name/nm0249291/?ref_=tt_ov_st",
-    },
-    {
-      id: "6360eac6e7653b1108a23197704",
-      firstName: "Sean",
-      lastName: "Harris",
-      gender: "male",
-      age: 48,
-      image:
-        "https://m.media-amazon.com/images/M/MV5BMTM3NTI1NzI4MF5BMl5BanBnXkFtZTcwODU1MTg4Nw@@._V1_.jpg",
-      link: "https://www.imdb.com/name/nm0365317/?ref_=tt_ov_st",
-      moviesCasted: 3,
-    },
-    {
-      id: "6360eb32e7653b111108a97705",
-      firstName: "Jada",
-      lastName: "Alberts",
-      gender: "female",
-      age: 46,
-      image:
-        "https://m.media-amazon.com/images/M/MV5BYTY0ZTU1YTItMTgyZS00YzVhLTg1ZGQtMzlkYTBiYTExMDZjXkEyXkFqcGdeQXVyMjgzMDAyMjY@._V1_UY1200_CR84,0,630,1200_AL_.jpg",
-      link: "https://www.imdb.com/name/nm3415084/?ref_=tt_ov_st",
-      moviesCasted: 1,
-    },
-    {
-      id: "6360ea98e7653b1108a1233977032",
-      firstName: "Joel",
-      lastName: "Edgerton",
-      gender: "male",
-      age: 48,
-      image:
-        "https://m.media-amazon.com/images/M/MV5BMTA0ODI1ODk4NzdeQTJeQWpwZ15BbWU3MDkwNjkzOTY@._V1_.jpg",
-      link: "https://www.imdb.com/name/nm0249291/?ref_=tt_ov_st",
-    },
-    {
-      id: "631260eac6e7653b1108a977042",
-      firstName: "Sean",
-      lastName: "Harris",
-      gender: "male",
-      age: 48,
-      image:
-        "https://m.media-amazon.com/images/M/MV5BMTM3NTI1NzI4MF5BMl5BanBnXkFtZTcwODU1MTg4Nw@@._V1_.jpg",
-      link: "https://www.imdb.com/name/nm0365317/?ref_=tt_ov_st",
-      moviesCasted: 3,
-    },
-    {
-      id: "6360eb32e7333653b1108a977053",
-      firstName: "Jada",
-      lastName: "Alberts",
-      gender: "female",
-      age: 46,
-      image:
-        "https://m.media-amazon.com/images/M/MV5BYTY0ZTU1YTItMTgyZS00YzVhLTg1ZGQtMzlkYTBiYTExMDZjXkEyXkFqcGdeQXVyMjgzMDAyMjY@._V1_UY1200_CR84,0,630,1200_AL_.jpg",
-      link: "https://www.imdb.com/name/nm3415084/?ref_=tt_ov_st",
-      moviesCasted: 1,
-    },
-  ];
+  const approvedReviews = selectedMovie?.movieReviews?.filter((review) => {
+    return review.isApproved;
+  });
 
-  const movie: IMovie = {
-    id: "6360eba7e7653b1108a97706",
-    title: "The Stranger",
-    description:
-      "Two men who meet on a plane and strike up a conversation that turns into friendship. For Henry Teague, worn down by a lifetime of physical labour and crime, this is a dream come true.",
-    image:
-      "https://m.media-amazon.com/images/M/MV5BYzhmNWMyYjQtNTVlMC00MGUwLWFmYjEtM2NkNWY0ODQ2YmFiXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg",
-    cost: 9000000,
-    yearReleased: 2022,
-  };
   return (
     <main className="pageContainer">
       <div className="sectionContainer">
@@ -100,48 +42,64 @@ const IndividualMovie = () => {
             <div className="movieDetailsLeft">
               <img
                 className="moviePoster"
-                src={movie.image}
-                alt={movie.title}
+                src={selectedMovie?.image}
+                alt={selectedMovie?.title}
               />
             </div>
             <div className="movieDetailsRight">
-              <h1 className="movieTitle">{movie.title}</h1>
+              <h1 className="movieTitle">{selectedMovie?.title}</h1>
               <div className="ratingContainer">
-                <MyRating rating={5} />
+                <MyRating rating={selectedMovie.rating as number} />
               </div>
-              <p className="movieDescription">{movie.description}</p>
+              <p className="movieDescription">{selectedMovie?.description}</p>
               <p className="movieCost">
-                <span>Budget Cost:</span>$ {budgetFormatter(movie.cost)}
+                <span>Budget Cost:</span>${" "}
+                {budgetFormatter(selectedMovie?.cost)}
               </p>
               <p className="movieYear">
                 <span>Year released:</span>
-                {movie.yearReleased}
+                {selectedMovie?.yearReleased}
               </p>
             </div>
           </div>
           <div className="movieActorsContainer">
             <h2 className="actorsPageH2">Actors/ Casts</h2>
-            <h1 className="noContentH1">This movie has no actors/ casts.</h1>
-            <div className="container">
-              <Carousel
-                mx="auto"
-                sx={{ minWidth: 1200, maxWidth: 1200 }}
-                loop
-                slideGap="md"
-              >
-                {actors.map((actor) => {
-                  return (
-                    <Carousel.Slide size="25%" gap="xl" key={actor.id}>
-                      <ActorCard actor={actor} />
-                    </Carousel.Slide>
-                  );
-                })}
-              </Carousel>
-            </div>
+            {(selectedMovie?.movieCasters &&
+              selectedMovie?.movieCasters.length <= 0) ||
+              (!selectedMovie?.movieCasters && (
+                <h1 className="noContentH1">
+                  This movie has no actors/ casts.
+                </h1>
+              ))}
+            {selectedMovie?.movieCasters && (
+              <div className="container">
+                <Carousel
+                  mx="auto"
+                  sx={{ minWidth: 1200, maxWidth: 1200 }}
+                  loop
+                  slideGap="md"
+                  align="start"
+                >
+                  {selectedMovie?.movieCasters?.map((actor) => {
+                    return (
+                      <Carousel.Slide size="25%" gap="xl" key={actor?.id}>
+                        <ActorCard actor={actor} isActorsPage={false} />
+                      </Carousel.Slide>
+                    );
+                  })}
+                </Carousel>
+              </div>
+            )}
           </div>
           <div className="movieReviewsContainer">
             <h2 className="reviewsPageH2">Reviews</h2>
-            <h1 className="noContentH1">This movie has no user reviews yet.</h1>
+            {((approvedReviews && (approvedReviews?.length as number) <= 0) ||
+              !approvedReviews) && (
+              <h1 className="noContentH1">
+                This movie has no user reviews yet.
+              </h1>
+            )}
+
             <div className="movieReviewForm">
               <Textarea
                 className="movieReviewFormWidth"
@@ -156,6 +114,9 @@ const IndividualMovie = () => {
                 autosize
                 minRows={2}
                 onChange={(e) => setReviewComment(e.target.value)}
+                error={
+                  reviewComment === "" && error && "Please enter your review."
+                }
               />
               <Rating
                 defaultValue={3}
@@ -174,18 +135,9 @@ const IndividualMovie = () => {
               </Button>
             </div>
             <div className="container">
-              <ReviewCard />
-              <ReviewCard />
-              <ReviewCard />
-              <ReviewCard />
-              <ReviewCard />
-              <ReviewCard />
-              <ReviewCard />
-              <ReviewCard />
-              <ReviewCard />
-              <ReviewCard />
-              <ReviewCard />
-              <ReviewCard />
+              {approvedReviews?.map((review) => {
+                return <ReviewCard review={review} key={review.id} />;
+              })}
             </div>
           </div>
         </div>
