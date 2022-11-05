60ec9e0dcd03cc768937ab72fbff6c8b5b1d1bdf
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-11-05 11:42:06 +0800

    separate dashboard page style

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,68 +1,21 @@
-import { AppShell, createStyles, Navbar } from "@mantine/core";
-import {
-  TableActors,
-  TableMovies,
-  TableReviews,
-  TableUsers,
-} from "../components";
+import { useState } from "react";
+import { AppShell, Navbar } from "@mantine/core";
 import {
   IconLayoutDashboard,
   IconUser,
   IconMovie,
   IconUsers,
 } from "@tabler/icons";
-import { useState } from "react";
-
-const useStyles = createStyles((theme) => ({
-  main: {
-    background: "#121212",
-    padding: 0,
-  },
-  link: {
-    ...theme.fn.focusStyles(),
-    display: "flex",
-    alignItems: "center",
-    textDecoration: "none !important",
-    fontSize: theme.fontSizes.sm,
-    color: theme.colors.gray[3],
-    padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
-    borderRadius: theme.radius.sm,
-    fontWeight: 500,
-
-    "&:hover": {
-      backgroundColor: theme.colors.dark[5],
-      color: theme.colors.blue[5],
-    },
-  },
-
-  linkIcon: {
-    color: theme.colors.gray[3],
-    marginRight: theme.spacing.sm,
-  },
-
-  linkActive: {
-    "&, &:hover": {
-      backgroundColor: theme.colors.blue[5],
-      color: theme.colors.gray[9],
-    },
-  },
-  left: {
-    display: "none",
-    [theme.fn.smallerThan("xs")]: {
-      display: "flex",
-      justifyContent: "flex-end",
-    },
-  },
-  navBar: {
-    padding: 25,
-    width: 300,
-    zIndex: 5,
-    backgroundColor: "#121212",
-  },
-}));
+import {
+  TableActors,
+  TableMovies,
+  TableReviews,
+  TableUsers,
+} from "../components";
+import { useDashboardPageStyles } from "../styles/DashboardPageStyles";
 
 const Dashboard = () => {
-  const { classes, cx } = useStyles();
+  const { classes, cx } = useDashboardPageStyles();
   const [active, setActive] = useState("Dashboard");
   const navItems = [
     { link: "", label: "Dashboard", icon: IconLayoutDashboard },
---
+++ frontend/src/styles/DashboardPageStyles.ts
@@ -0,0 +1,49 @@
+import { createStyles } from "@mantine/core";
+
+export const useDashboardPageStyles = createStyles((theme) => ({
+  main: {
+    background: "#121212",
+    padding: 0,
+  },
+  link: {
+    ...theme.fn.focusStyles(),
+    display: "flex",
+    alignItems: "center",
+    textDecoration: "none !important",
+    fontSize: theme.fontSizes.sm,
+    color: theme.colors.gray[3],
+    padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
+    borderRadius: theme.radius.sm,
+    fontWeight: 500,
+
+    "&:hover": {
+      backgroundColor: theme.colors.dark[5],
+      color: theme.colors.blue[5],
+    },
+  },
+
+  linkIcon: {
+    color: theme.colors.gray[3],
+    marginRight: theme.spacing.sm,
+  },
+
+  linkActive: {
+    "&, &:hover": {
+      backgroundColor: theme.colors.blue[5],
+      color: theme.colors.gray[9],
+    },
+  },
+  left: {
+    display: "none",
+    [theme.fn.smallerThan("xs")]: {
+      display: "flex",
+      justifyContent: "flex-end",
+    },
+  },
+  navBar: {
+    padding: 25,
+    width: 300,
+    zIndex: 5,
+    backgroundColor: "#121212",
+  },
+}));
