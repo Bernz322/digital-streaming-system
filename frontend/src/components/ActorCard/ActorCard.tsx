b61c2ee4d6c6a67844a90200fcbd37c41e1a7dac
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-11-06 15:25:48 +0800

    export interface and add tests

--- frontend/src/components/ActorCard/ActorCard.tsx
+++ frontend/src/components/ActorCard/ActorCard.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { IActor } from "../../utils/types";
 import "./ActorCard.scss";
 
-interface ActorProps {
+export interface ActorProps {
   actor: IActor;
   isActorsPage: boolean;
 }
