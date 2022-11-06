430087ab5aff0d0d5e9ea2c47ee5e96c96897a9f
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-11-07 02:01:24 +0800

    add more handlers and add config for tests

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,24 +1,19 @@
 import { rest } from "msw";
 import { baseAPIUrl } from "../utils/apiCalls";
 import {
+  mockActor,
   mockActors,
+  mockLoginResponseData,
+  mockMovie,
   mockMovies,
+  mockRegisterResponseData,
   mockSearchedActor,
   mockSearchedMovies,
+  mockUsers,
 } from "../utils/db.mocks";
 
 export const handlers = [
-  rest.get(`${baseAPIUrl}/movies`, (req, res, ctx) => {
-    return res(
-      ctx.json({
-        status: "success",
-        data: mockMovies,
-        message: "Successfully fetched all movies.",
-      }),
-      ctx.delay(150)
-    );
-  }),
-  rest.get(`/search/movies/:title`, (req, res, ctx) => {
+  rest.get(`${baseAPIUrl}/search/movies/:title`, (req, res, ctx) => {
     const { title } = req.params;
     switch (title) {
       case "john": {
@@ -62,17 +57,28 @@ export const handlers = [
         );
     }
   }),
-  rest.get(`${baseAPIUrl}/actors`, (req, res, ctx) => {
+  rest.get(`${baseAPIUrl}/movies`, (req, res, ctx) => {
     return res(
       ctx.json({
         status: "success",
-        data: mockActors,
-        message: "Successfully fetched all actors in the database..",
+        data: mockMovies,
+        message: "Successfully fetched all movies.",
       }),
       ctx.delay(150)
     );
   }),
-  rest.get(`/search/actors/:name`, (req, res, ctx) => {
+  rest.get(`${baseAPIUrl}/movies/:movieId`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        status: "success",
+        data: mockMovie,
+        message: "Successfully fetched all movies.",
+      }),
+      ctx.delay(150)
+    );
+  }),
+
+  rest.get(`${baseAPIUrl}/search/actors/:name`, (req, res, ctx) => {
     const { name } = req.params;
     switch (name) {
       case "keanu": {
@@ -105,5 +111,68 @@ export const handlers = [
           ctx.delay(150)
         );
     }
+  }),
+  rest.get(`${baseAPIUrl}/actors`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        status: "success",
+        data: mockActors,
+        message: "Successfully fetched all actors in the database.",
+      }),
+      ctx.delay(150)
+    );
+  }),
+  rest.get(`${baseAPIUrl}/actors/:actorId`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        status: "success",
+        data: mockActor,
+        message: "Successfully fetched actor data.",
+      }),
+      ctx.delay(150)
+    );
+  }),
+
+  rest.get(`${baseAPIUrl}/users`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        status: "success",
+        data: mockUsers,
+        message: "All users data fetched successfully.",
+      }),
+      ctx.delay(150)
+    );
+  }),
+
+  rest.post(`${baseAPIUrl}/users/login`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        status: "success",
+        data: mockLoginResponseData,
+        message: "User logged in successfully.",
+      }),
+      ctx.delay(150)
+    );
+  }),
+  rest.post(`${baseAPIUrl}/users/register`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        status: "success",
+        data: mockRegisterResponseData,
+        message: "User registered successfully.",
+      }),
+      ctx.delay(150)
+    );
+  }),
+
+  rest.post(`${baseAPIUrl}/reviews`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        status: "success",
+        data: null,
+        message: "Movie review successfully added.",
+      }),
+      ctx.delay(150)
+    );
   }),
 ];
