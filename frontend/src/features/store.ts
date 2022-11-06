c4bb894e70183cca81907892325bc605fa86d349
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-11-06 15:22:30 +0800

    add test utils from rtk documentation and configure redux store

--- frontend/src/features/store.ts
+++ frontend/src/features/store.ts
@@ -1,18 +1,31 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {
+  combineReducers,
+  configureStore,
+  PreloadedState,
+} from "@reduxjs/toolkit";
 
 import authReducer from "./auth/authSlice";
 import movieReducer from "./movie/movieSlice";
 import actorReducer from "./actor/actorSlice";
 import userReducer from "./user/userSlice";
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    movie: movieReducer,
-    actor: actorReducer,
-    user: userReducer,
-  },
+// Create the root reducer independently to obtain the RootState type
+const rootReducer = combineReducers({
+  auth: authReducer,
+  movie: movieReducer,
+  actor: actorReducer,
+  user: userReducer,
 });
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export const createStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    preloadedState,
+  });
+};
+
+export const store = createStore();
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof createStore>;
+export type AppDispatch = AppStore["dispatch"];
