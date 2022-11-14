b418f2674f9a64475d5d316deb8f6d6c5db44c27
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2022-11-14 10:28:28 +0800

    add unit test

--- backend/src/__tests__/unit/actors.controller.unit.ts
+++ backend/src/__tests__/unit/actors.controller.unit.ts
@@ -61,32 +61,41 @@ describe('Actors Controller', () => {
     });
     it('should return failed response if first name is empty', async () => {
       const actor = newActorBody({...mockActor, firstName: ''});
-      const message = 'Field firstName name is required.';
+      const expectedMessage = 'Field firstName name is required.';
       const create = actorRepository.stubs.create;
 
       create.resolves(actor);
       const result = await controller.create(actor);
 
-      expect(result).to.eql(failedRes(message));
+      expect(result).to.eql(failedRes(expectedMessage));
     });
     it('should return failed response if gender is not valid', async () => {
       const actor = newActorBody({...mockActor, gender: 'notAGender'});
-      const message = 'Gender should only be either male or female';
+      const expectedMessage = 'Gender should only be either male or female';
       const create = actorRepository.stubs.create;
 
       create.resolves(actor);
       const result = await controller.create(actor);
 
-      expect(result).to.eql(failedRes(message));
+      expect(result).to.eql(failedRes(expectedMessage));
     });
     it('should return failed response if age is less than 1', async () => {
       const actor = newActorBody({...mockActor, age: 0});
-      const message = 'Actor age cannot be less than a year.';
+      const expectedMessage = 'Actor age cannot be less than a year.';
       const create = actorRepository.stubs.create;
 
       create.resolves(actor);
       const result = await controller.create(actor);
-      expect(result).to.eql(failedRes(message));
+      expect(result).to.eql(failedRes(expectedMessage));
+    });
+    it('should return custom message failed response if rejected', async () => {
+      const actor = newActorBody(mockActor);
+      const expectedMessage = 'Adding new Actor failed.';
+      const create = actorRepository.stubs.create;
+
+      create.rejects({message: expectedMessage});
+      const result = await controller.create(actor);
+      expect(result).to.eql(failedRes(expectedMessage));
     });
   });
 
@@ -99,7 +108,6 @@ describe('Actors Controller', () => {
 
       sinon.assert.called(findById);
     });
-
     it('should return failed response if rejected', async () => {
       const findById = actorRepository.stubs.findById;
       const actorId = '6365cd4ee303fc6228363b9f';
@@ -120,6 +128,142 @@ describe('Actors Controller', () => {
       );
 
       sinon.assert.called(searchByName);
+    });
+    it('should return custom message failed response if rejected', async () => {
+      const searchByName = actorRepository.stubs.find;
+      const searchKey = 'Non existing';
+      searchByName.rejects({message: 'Fetching actor data failed.'});
+      expect(await controller.searchByName(searchKey)).to.eql(
+        failedRes('Fetching actor data failed.'),
+      );
+
+      sinon.assert.called(searchByName);
+    });
+  });
+
+  describe('Actors Controller updateById', () => {
+    it('should return failed response if first name is null', async () => {
+      const actor = newActorBody({...mockActor, firstName: ''});
+      const expectedMessage = 'Field firstName should not be empty.';
+      const actorId = '636da2be886fb224fc3b2fd1';
+      const updateById = actorRepository.stubs.updateById;
+
+      updateById.resolves();
+      const result = await controller.updateById(actorId, actor);
+      expect(result).to.eql(failedRes(expectedMessage));
+    });
+    it('should return failed response if first name is invalid', async () => {
+      const actor = newActorBody({...mockActor, firstName: 'N4me-1s-1nval1d'});
+      const expectedMessage = 'Invalid name in field firstName';
+      const actorId = '636da2be886fb224fc3b2fd1';
+      const updateById = actorRepository.stubs.updateById;
+
+      updateById.resolves();
+      const result = await controller.updateById(actorId, actor);
+      expect(result).to.eql(failedRes(expectedMessage));
+    });
+    it('should return failed response if last name is null', async () => {
+      const actor = newActorBody({...mockActor, lastName: ''});
+      const expectedMessage = 'Field lastName should not be empty.';
+      const actorId = '636da2be886fb224fc3b2fd1';
+      const updateById = actorRepository.stubs.updateById;
+
+      updateById.resolves();
+      const result = await controller.updateById(actorId, actor);
+      expect(result).to.eql(failedRes(expectedMessage));
+    });
+    it('should return failed response if last name is invalid', async () => {
+      const actor = newActorBody({...mockActor, lastName: 'N4me-1s-1nval1d'});
+      const expectedMessage = 'Invalid name in field lastName';
+      const actorId = '636da2be886fb224fc3b2fd1';
+      const updateById = actorRepository.stubs.updateById;
+
+      updateById.resolves();
+      const result = await controller.updateById(actorId, actor);
+      expect(result).to.eql(failedRes(expectedMessage));
+    });
+    it('should return failed response if gender is neither male or female', async () => {
+      const actor = newActorBody({...mockActor, gender: 'notAGender'});
+      const expectedMessage = 'Gender should only be either male or female';
+      const actorId = '636da2be886fb224fc3b2fd1';
+      const updateById = actorRepository.stubs.updateById;
+
+      updateById.resolves();
+      const result = await controller.updateById(actorId, actor);
+      expect(result).to.eql(failedRes(expectedMessage));
+    });
+    it('should return failed response if age is less than 1', async () => {
+      const actor = newActorBody({...mockActor, age: 0});
+      const expectedMessage = 'Actor age cannot be less than a year.';
+      const actorId = '636da2be886fb224fc3b2fd1';
+      const updateById = actorRepository.stubs.updateById;
+
+      updateById.resolves();
+      const result = await controller.updateById(actorId, actor);
+      expect(result).to.eql(failedRes(expectedMessage));
+    });
+    it('should return failed response if image is null', async () => {
+      const actor = newActorBody({...mockActor, image: ''});
+      const expectedMessage = 'Field image should not be empty.';
+      const actorId = '636da2be886fb224fc3b2fd1';
+      const updateById = actorRepository.stubs.updateById;
+
+      updateById.resolves();
+      const result = await controller.updateById(actorId, actor);
+      expect(result).to.eql(failedRes(expectedMessage));
+    });
+    it('should return failed response if image is invalid', async () => {
+      const actor = newActorBody({...mockActor, image: 'invalid-url'});
+      const expectedMessage = 'Invalid actor image url.';
+      const actorId = '636da2be886fb224fc3b2fd1';
+      const updateById = actorRepository.stubs.updateById;
+
+      updateById.resolves();
+      const result = await controller.updateById(actorId, actor);
+      expect(result).to.eql(failedRes(expectedMessage));
+    });
+    it('should return failed response if actor link is invalid', async () => {
+      const actor = newActorBody({...mockActor, link: 'invalid-url'});
+      const expectedMessage = 'Invalid actor link url.';
+      const actorId = '636da2be886fb224fc3b2fd1';
+      const updateById = actorRepository.stubs.updateById;
+
+      updateById.resolves();
+      const result = await controller.updateById(actorId, actor);
+      expect(result).to.eql(failedRes(expectedMessage));
+    });
+    it('should successfully update actor', async () => {
+      const actor = newActorBody({...mockActor, firstName: 'Edited Name'});
+      const actorId = '636da2be886fb224fc3b2fd1';
+      const updateById = actorRepository.stubs.updateById;
+
+      updateById.resolves();
+      await controller.updateById(actorId, actor);
+
+      sinon.assert.calledWith(updateById, actorId, actor);
+    });
+    it('should return custom message failed response if rejected', async () => {
+      const actor = newActorBody({...mockActor, firstName: 'Edited Name'});
+      const id = 'invalidId';
+      const expectedMessage = 'Updating actor failed';
+      const updateById = actorRepository.stubs.updateById;
+
+      updateById.rejects({message: expectedMessage});
+      await controller.updateById(id, actor);
+
+      expect(await controller.updateById(id, actor)).to.eql(
+        failedRes(expectedMessage),
+      );
+    });
+  });
+
+  describe('Actors Controller delete', () => {
+    it('should successfully delete actor', async () => {
+      const deleteById = actorRepository.stubs.deleteById;
+      const actorId = '636da2be886fb224fc3b2fd1';
+
+      deleteById.resolves();
+      await controller.deleteById(actorId);
     });
   });
 
